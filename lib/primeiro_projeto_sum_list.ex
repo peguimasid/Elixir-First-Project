c4940f19b789be6e0ec7337c1d9b165d03879c69
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-10 18:14:25 -0300

    usando o modulo Enum

--- lib/primeiro_projeto_sum_list.ex
+++ lib/primeiro_projeto_sum_list.ex
@@ -1,6 +1,8 @@
 defmodule PrimeiroProjetoSumList do
   def call(list), do: sum(list, 0)
 
+  def sumWithEnum(list), do: Enum.sum(list)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do

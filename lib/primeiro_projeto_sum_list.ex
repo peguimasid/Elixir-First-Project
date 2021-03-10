b8e53596ec66e3df0a19c656cb89666ffa4f2ced
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-10 15:22:34 -0300

    starting project

---
+++ lib/primeiro_projeto_sum_list.ex
@@ -0,0 +1,18 @@
+defmodule PrimeiroProjetoSumList do
+  @moduledoc """
+  Documentation for `PrimeiroProjetoSumList`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrimeiroProjetoSumList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/primeiro_projeto_sum_list_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimeiroProjetoSumListTest do
+  use ExUnit.Case
+  doctest PrimeiroProjetoSumList
+
+  test "greets the world" do
+    assert PrimeiroProjetoSumList.hello() == :world
+  end
+end
